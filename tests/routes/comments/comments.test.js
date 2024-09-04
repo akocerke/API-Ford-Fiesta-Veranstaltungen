4064601ab00dc070036cdc7af70f92b36d9313b5
akocerke <152269613+akocerke@users.noreply.github.com>
2024-09-04 12:53:51 +0200

    Unit-Tests für alle GET /all-Routen der API erstellt

--- tests/routes/comments/comments.test.js
+++ tests/routes/comments/comments.test.js
@@ -1,15 +1,22 @@
 // tests/routes/comments/comments.test.js
 const request = require('supertest');
-const app = require('../../../index'); // Importieren Sie die Express-Anwendung
+const app = require('../../../index');
 
 describe('GET /api-ford-fiesta/comments/all', () => {
-  test('sollte eine Liste von Kommentaren zurückgeben', async () => {
-    const response = await request(app)
+  test('sollte eine Liste von Kommentaren mit Status 200 zurückgeben', async () => {
+    const { body: comments } = await request(app)
       .get('/api-ford-fiesta/comments/all')
       .expect('Content-Type', /json/)
       .expect(200);
 
-    const myComments = response.body;
-    expect(Array.isArray(myComments)).toBe(true);
+    expect(Array.isArray(comments)).toBe(true);
+  });
+});
+
+describe('GET /api-ford-fiesta/comments/all (Fehlerfälle)', () => {
+  test('sollte 404 für eine ungültige Route zurückgeben', async () => {
+    await request(app)
+      .get('/api-ford-fiesta/comments/invalidroute')
+      .expect(404);
   });
 });
