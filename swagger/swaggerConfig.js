3a3d108cf9d26aa36227237a1f3c2efc431722dc
akocerke <152269613+akocerke@users.noreply.github.com>
2024-09-09 12:40:44 +0200

    swagger dokumentation mit authToken funktioniert

--- swagger/swaggerConfig.js
+++ swagger/swaggerConfig.js
@@ -1,32 +1,38 @@
-const swaggerJsDoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
 const path = require('path');
 
-const swaggerOptions = {
-  swaggerDefinition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'API FORD FIESTA VERANSTALTUNGEN',
-      version: '1.0.0',
-      description: 'API Dokumentation mit Swagger',
-    },
-    servers: [
-      {
-        url: 'http://localhost:5050/api-ford-fiesta',
-        description: 'Entwicklungsserver',
-      },
-    ],
-  },
-  apis: [
-    path.join(__dirname, 'admins.js'),
-    path.join(__dirname, 'users.js'),
-    path.join(__dirname, 'comments.js'),
-    path.join(__dirname, 'events.js'),
-    path.join(__dirname, 'ratings.js'),
-    path.join(__dirname, 'violations.js'),
-  ],
-};
+// YAML-Dateien laden
+const swaggerAuth = YAML.load(path.join(__dirname, 'auth.yml'));
+const swaggerAdmin = YAML.load(path.join(__dirname, 'admins.yml'));
 
-const swaggerDocs = swaggerJsDoc(swaggerOptions);
+// Swagger-Dokumentation zusammenführen
+const swaggerDocs = {
+  openapi: '3.0.0',
+  info: {
+    title: 'API FORD FIESTA VERANSTALTUNGEN',
+    version: '1.0.0',
+    description: 'API Dokumentation mit Swagger',
+  },
+  servers: [
+    {
+      url: 'http://localhost:5050/api-ford-fiesta',
+      description: 'Entwicklungsserver',
+    },
+  ],
+  components: {
+    securitySchemes: {
+      bearerAuth: {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+      },
+    },
+  },
+  paths: {
+    ...swaggerAuth.paths,
+    ...swaggerAdmin.paths,
+  },
+};
 
 module.exports = { swaggerUi, swaggerDocs };
