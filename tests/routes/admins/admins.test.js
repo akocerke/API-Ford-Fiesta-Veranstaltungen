3f2379b38a60db33cee2361af413b2b7f9195520
akocerke <152269613+akocerke@users.noreply.github.com>
2024-09-10 08:04:24 +0200

    admin-tests update

--- tests/routes/admins/admins.test.js
+++ tests/routes/admins/admins.test.js
@@ -2,10 +2,24 @@
 const request = require('supertest');
 const app = require('../../../index');
 
+let token;
+
+beforeAll(async () => {
+  const res = await request(app)
+    .post('/api-ford-fiesta/auth/login') // Pfad zum Login-Endpunkt
+    .send({
+      email: 'testuser@example.com',
+      password: 'Test1234!',
+    });
+
+  token = res.body.token; // Token aus der Login-Antwort speichern
+});
+
 describe('GET /api-ford-fiesta/admins/admins', () => {
   test('sollte eine Liste von Admins mit Status 200 zurückgeben', async () => {
     const { body: admins } = await request(app)
       .get('/api-ford-fiesta/admins/admins')
+      .set('Authorization', `Bearer ${token}`) // Generierten Token setzen
       .expect('Content-Type', /json/)
       .expect(200);
 
@@ -15,12 +29,13 @@ describe('GET /api-ford-fiesta/admins/admins', () => {
 
 describe('GET /api-ford-fiesta/admins/users', () => {
   test('sollte eine Liste von allen Users mit Status 200 zurückgeben', async () => {
-    const { body: admins } = await request(app)
+    const { body: users } = await request(app)
       .get('/api-ford-fiesta/admins/users')
+      .set('Authorization', `Bearer ${token}`) // Generierten Token setzen
       .expect('Content-Type', /json/)
       .expect(200);
 
-    expect(Array.isArray(admins)).toBe(true);
+    expect(Array.isArray(users)).toBe(true);
   });
 });
 
