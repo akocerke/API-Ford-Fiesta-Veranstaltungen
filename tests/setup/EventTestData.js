efb7e7c3f3c85c7f072f510af1da427d5bf5eea8
akocerke <152269613+akocerke@users.noreply.github.com>
2024-10-05 10:40:48 +0200

    workflow

--- tests/setup/EventTestData.js
+++ tests/setup/EventTestData.js
@@ -1,21 +1,21 @@
 // tests/setup/EventTestData.js
 const EventTestdata = [
   {
-    user_id: 1, // Referenziert den Benutzer mit ID 1
+    userId: 1, // Referenziert den Benutzer mit ID 1
     title: 'Ford Fiesta Treffen',
     description: 'Ein Treffen für alle Ford Fiesta-Enthusiasten!',
     date: new Date('2024-10-15T10:00:00Z'), // Zukunftstermin
     image: 'fiesta-treffen.jpg',
   },
   {
-    user_id: 1, // Referenziert den Benutzer mit ID 1
+    userId: 1, // Referenziert den Benutzer mit ID 1
     title: 'Reparatur-Workshop',
     description: 'Lerne, wie man den Ford Fiesta wartet und repariert.',
     date: new Date('2024-11-20T14:00:00Z'), // Zukunftstermin
     image: 'fiesta-workshop.jpg',
   },
   {
-    user_id: 2, // Referenziert den Benutzer mit ID 2
+    userId: 2, // Referenziert den Benutzer mit ID 2
     title: 'Sonntagsausfahrt',
     description:
       'Eine entspannte Ausfahrt durch die Landschaft mit dem Ford Fiesta.',
